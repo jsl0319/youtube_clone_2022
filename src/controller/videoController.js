3bf909ddfc549bb4003469e9fe4464e61997c2aa
kokonb0 <kokonb0@efit.co.kr>
2021-12-29 11:25:55 +0900

    feet: pug 추가

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,7 +1,7 @@
 
-export const trending = (req, res) => {res.send('trending')};
+export const trending = (req, res) => {res.render("home")};
 export const search = (req, res) => {res.send('search')};
-export const see = (req,res) => {res.send('see')};
+export const see = (req,res) => {res.render("watch")};
 export const edit = (req,res) => {res.send('edit video')};
 export const deleteVideo = (req,res) => {res.send('deleteVideo')};
 export const upload = (req,res) => {res.send('upload video')};
--- src/server.js
+++ src/server.js
@@ -8,13 +8,19 @@ const app = express();
 const PORT = 4000; 
 const logger = morgan('dev');
 
+// view engine 설정 , views 디폴트 경로 설정
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views")
+
+// log 설정
 app.use(logger);
 
+// router 시작점
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
+// server listener
 const handleListener = () => console.log(`Server listening on http://172.30.1.180:${PORT} 🎉`);
-
 app.listen(PORT, handleListener);
 
