bf5d8b033b1c1d9c03d3159abd1d1d2bed9caff7
kokonb0 <kokonb0@efit.co.kr>
2022-01-05 18:30:29 +0900

    mongoose findById

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,9 +1,13 @@
+/**
+ * Video Controller - Video CRUD!
+ */
 import Video from "../models/Video"
 
+// ============= GET =============
+// 전체 목록 page
 export const home = async (req, res) => {
     try{
         const videos = await Video.find({});
-        console.log(videos);
         return res.render("home", {pageTitle : "Home", videos})
     }
     catch{
@@ -12,24 +16,29 @@ export const home = async (req, res) => {
 };
 
 export const search = (req, res) => {res.send('search')};
-
-export const watch = (req,res) => {
-    return res.render("watch")
+// 상세 page
+export const watch = async (req,res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    return res.render("watch", { pageTitle: video.title, video })
 };
-
+// 수정 page
 export const getEdit = (req,res) => {
     return res.render('edit')
 };
-
-export const postEdit = (req,res) => {
-    return res.redirect(`/videos/${ id }`)
-};
-
+// 업로드 page
 export const getUpload = (req, res) => {
     let pageTitle = 'Upload Video';
     return res.render("upload", { pageTitle })
 };
 
+
+// =========== POST =============
+// 수정
+export const postEdit = (req,res) => {
+    return res.redirect(`/videos/${ id }`)
+};
+// 업로드
 export const postUpload = async (req, res) => {
     const {title, description, hashtags} = req.body;
     try {
@@ -47,9 +56,8 @@ export const postUpload = async (req, res) => {
     }
     catch(error) {
         let pageTitle = 'Upload Video';
-        console.log(error);
         return res.render("upload", { pageTitle, errorMessage : error._message})
     }
 };
-
+// 삭제
 export const deleteVideo = (req,res) => {return res.send('deleteVideo')};
