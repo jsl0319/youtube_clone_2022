121d632ab93ccd99a81208d27333863d8f662fbd
jslee <kokonb0@efit.co.kr>
2022-12-09 16:34:36 +0900

    fix: arrow fn -> funtion 변경

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
+import { isEmpty } from "../../utils";
 
 // Schema 생성.
 const UserSchema = new mongoose.Schema({
@@ -13,13 +14,15 @@ const UserSchema = new mongoose.Schema({
 });
 
 // 가입시 Password 해싱
-UserSchema.pre("save", async () => {
-  console.log("input password::", this.password);
-  this.password = await bcrypt.hash(this.password, 5);
+UserSchema.pre("save", async function() {
+  console.log("input password::", this);
+  // 깃헙 회원가입은 패스
+  if(!isEmpty(this.password))
+    this.password = await bcrypt.hash(this.password, 5);
   console.log("hashed password::", this.password);
 });
 
-// Model 생성 - Video 모델 생성
+// Model 생성 - User 모델 생성
 const User = mongoose.model("User", UserSchema);
 
 export default User;
